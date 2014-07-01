4d8f971002ca791aa3aced2884b14d70ddad1a76
Mathieu Perreault <mathieu.perreault@gmail.com>
2014-07-01 18:45:32 -0400

    fixing server

--- server.js
+++ server.js
@@ -85,6 +85,11 @@ app.get('/auth/nest/callback',
         }
        );
 
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 /**
   Export the app
 */
